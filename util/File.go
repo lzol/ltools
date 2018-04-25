8a2368db2e69d1bffb24e8387055f4bab66d38b4
liuz <lzol@139.com>
2018-04-25 20:46:52 +0800

    添加获取文件路径、名称、扩展名、全路径方法

--- util/File.go
+++ util/File.go
@@ -1,6 +1,9 @@
 package util
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 func CreateNewFile(fileName string)(file *os.File,err error){
 	_, err = os.Stat(fileName)
@@ -12,4 +15,33 @@ func CreateNewFile(fileName string)(file *os.File,err error){
 		return nil,err
 	}
 	return file,nil
+}
+
+/*
+	获取文件名称，不含路径
+*/
+func GetFileName(fileWholePath string)( fileName string){
+	return filepath.Base(fileWholePath)
+}
+
+/*
+	获取文件路径，不含名称
+*/
+func GetFilePath(fileWholePath string)( filePath string){
+	return filepath.Dir(fileWholePath)
+}
+
+/*
+	获取文件路径，不含名称
+*/
+func GetFileExt(fileWholePath string)( fileExt string){
+	return filepath.Ext(fileWholePath)
+}
+
+/*
+	获取文件全路径，含名称
+*/
+func GetFileWholePath(fileName string)( fileWholePath string,err error){
+	fileWholePath,err = filepath.Abs(fileName)
+	return fileWholePath,err
 }
