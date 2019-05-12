2da79c951982ce41299c7e379a20b3bf28e3e1d7
lzol <lzol@139.com>
2019-05-12 21:59:09 +0800

    修改AES算法实现方式，添加IV

--- util/secret/Aes.go
+++ util/secret/Aes.go
@@ -6,41 +6,50 @@ import (
 	"encoding/base64"
 )
 
-func AesEncrypt(orig, key, iv string) string {
+func AesEncrypt(orig, key, iv string) (ret string, err error) {
 	// 转成字节数组
 	origData := []byte(orig)
 	k := []byte(key)
-	IV := []byte(iv)
+
 	// 分组秘钥
-	block, _ := aes.NewCipher(k)
+	block, err := aes.NewCipher(k)
+	if err != nil {
+		return ret, err
+	}
 	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
 	// 补全码
 	origData = PKCS7Padding(origData, blockSize)
 	// 加密模式
-	blockMode := cipher.NewCBCEncrypter(block, IV)
+	blockMode := cipher.NewCBCEncrypter(block, []byte(iv))
 	// 创建数组
 	cryted := make([]byte, len(origData))
 	// 加密
 	blockMode.CryptBlocks(cryted, origData)
-	return base64.StdEncoding.EncodeToString(cryted)
-
+	ret = base64.URLEncoding.EncodeToString(cryted)
+	return ret, nil
 }
 
-func AesDecrypt(cryted, key,iv string) string {
+func AesDecrypt(cryted, key, iv string) (ret string, err error) {
 	// 转成字节数组
-	crytedByte, _ := base64.StdEncoding.DecodeString(cryted)
+	crytedByte, err := base64.URLEncoding.DecodeString(cryted)
+	if err != nil {
+		return ret, err
+	}
 	k := []byte(key)
-	IV := []byte(iv)
 	// 分组秘钥
-	block, _ := aes.NewCipher(k)
+	block, err := aes.NewCipher(k)
+	if err != nil {
+		return ret, err
+	}
 	// 加密模式
-	blockMode := cipher.NewCBCDecrypter(block, IV)
+	blockMode := cipher.NewCBCDecrypter(block, []byte(iv))
 	// 创建数组
 	orig := make([]byte, len(crytedByte))
 	// 解密
 	blockMode.CryptBlocks(orig, crytedByte)
 	// 去补全码
 	orig = PKCS7UnPadding(orig)
-	return string(orig)
+	ret = string(orig)
+	return ret, nil
 }
